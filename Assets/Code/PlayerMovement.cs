cd0b955e2bff2af811c8f991d8ebd8aaae9adbb9
Lucas Leonel Siqueira <lucasleosiq@gmail.com>
2023-06-27 11:56:28 -0300

    feat: add player movement

--- Assets/Code/PlayerMovement.cs
+++ Assets/Code/PlayerMovement.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed = 10f;
+    public Rigidbody rb;
+
+    public float minY = -2f;
+    public float maxY = 2f;
+
+    void FixedUpdate()
     {
-        
+        PlayerYMovement();
+        MovementLimit();
     }
 
-    // Update is called once per frame
-    void Update()
+    void PlayerYMovement()
     {
-        
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(0f, verticalInput, 0f);
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+
+    void MovementLimit()
+    {
+        Vector3 currentPosition = transform.position;
+        currentPosition.y = Mathf.Clamp(currentPosition.y, minY, maxY);
+        transform.position = currentPosition;
     }
 }
