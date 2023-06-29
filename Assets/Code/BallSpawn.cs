b54ca792fd213a3dfb655b2aa63df8df0b74c7ff
Lucas Leonel Siqueira <lucasleosiq@gmail.com>
2023-06-29 17:42:31 -0300

    feat: add bounce when interacting with paddle and field bounds

--- Assets/Code/BallSpawn.cs
+++ Assets/Code/BallSpawn.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class BallSpawn : MonoBehaviour
 {
-    public int speed = 5;
+    public float speedY = 2;
+    public float speedX = 5;
     public GameObject thisObject;
 
     void Start()
@@ -15,14 +16,37 @@ public class BallSpawn : MonoBehaviour
 
     void Update()
     {
-        transform.Translate(Vector3.right * speed * Time.deltaTime);
+        transform.Translate(Vector3.right * speedX * Time.deltaTime);
+        transform.Translate(Vector3.down * speedY * Time.deltaTime);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Bounce"))
         {
-            Debug.Log("Bounce Trigger Set"); 
+            speedY = speedY * -1;
+            speedX = speedX * -1;
         }
+
+        if (other.CompareTag("Boundary"))
+        {
+            speedY = speedY * -1;
+        }
+
+        else if (other.CompareTag("UpperPaddle"))
+        {
+
+        }
+
+        else if (other.CompareTag("MiddlePaddle"))
+        {
+
+        }
+
+        else if (other.CompareTag("LowerPaddle"))
+        {
+
+        }
+
     }
-}
+}   
