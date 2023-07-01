d67623f369a3840a3cd271179c37a398fb2e1f5c
Lucas Leonel Siqueira <lucasleosiq@gmail.com>
2023-07-01 14:17:27 -0300

    fix: ball bouncing incorrectly in paddle

--- Assets/Code/Ball/BallBounce.cs
+++ Assets/Code/Ball/BallBounce.cs
@@ -1,12 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BallSpawn : MonoBehaviour
+public class BallBounce : MonoBehaviour
 {
     public float speedY = 2;
     public float speedX = 5;
     public GameObject thisObject;
+    
+    private bool bounceCooldown = false;
 
     void Start()
     {
@@ -28,30 +31,43 @@ public class BallSpawn : MonoBehaviour
             Debug.Log("Tag: Boundary");
         }
 
-        else if (other.CompareTag("UpperPaddle"))
-        {
-            speedX *= -1;
-            speedY = 1;
-            Debug.Log("Tag: Upper Paddle");
-        }
-
-        else if (other.CompareTag("MiddlePaddle"))
-        {
-            speedX *= -1;
-            speedY = 0;
-            Debug.Log("Tag: Middle Paddle");
-        }
-
-        else if (other.CompareTag("LowerPaddle"))
+        else if (other.CompareTag("UpperPaddle") && (bounceCooldown == false))
         {
             speedX *= -1;
             speedY = -1;
-            Debug.Log("Tag: Lower Paddle");
+            StartCoroutine(ActivateBounceCooldown());
+            Debug.Log("Tag: Upper Paddle");
         }
 
+        else if (other.CompareTag("MiddlePaddle") && (bounceCooldown == false))
+        {
+            speedX *= -1;
+            speedY = 0;
+            StartCoroutine(ActivateBounceCooldown());
+            Debug.Log("Tag: Middle Paddle");
+        }
+
+        else if (other.CompareTag("LowerPaddle") && (bounceCooldown == false))
+        {
+            speedX *= -1;
+            speedY = 1;
+            StartCoroutine(ActivateBounceCooldown());
+            Debug.Log("Tag: Lower Paddle");
+        }
     }
 
-    [NaughtyAttributes.Bi]
+    private IEnumerator ActivateBounceCooldown()
+    {
+        bounceCooldown = true;
+        Debug.Log("BounceCooldown activated!");
+
+        yield return new WaitForSeconds(1f);
+
+        bounceCooldown = false;
+        Debug.Log("BounceCooldown over!");
+    }
+
+    [NaughtyAttributes.Button]
     void ResetBall()
     {
         transform.position = Vector3.zero;
