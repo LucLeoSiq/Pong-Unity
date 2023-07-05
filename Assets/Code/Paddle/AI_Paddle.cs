a99fa148464be6b47d1684554e14ab4b64371d14
Lucas Leonel Siqueira <lucasleosiq@gmail.com>
2023-07-05 20:45:00 -0300

    feat: AI paddle logic and movement

---
+++ Assets/Code/Paddle/AI_Paddle.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+public class AI_Paddle : MonoBehaviour
+{
+    public Transform ballTransform;
+    public float paddleSpeed = 10f;
+
+    public Rigidbody2D paddleRb;
+
+    [Header("Paddle Movement Boundary")]
+    public float minY = -5f;
+    public float maxY = 5f;
+
+    private void Start()
+    {
+        paddleRb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        // Calculate the target position based on the ball's y-position
+        Vector2 targetPosition = new Vector2(transform.position.x, ballTransform.position.y);
+
+        paddleRb.MovePosition(Vector2.Lerp(transform.position, targetPosition, Time.fixedDeltaTime * paddleSpeed));
+
+        MovementLimit();
+    }
+
+    void MovementLimit()
+    {
+        Vector3 currentPosition = transform.position;
+        currentPosition.y = Mathf.Clamp(currentPosition.y, minY, maxY);
+        transform.position = currentPosition;
+    }
+}
