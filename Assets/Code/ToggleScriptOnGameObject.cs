836a6508f385559bcbf38081d553961343edc71a
Lucas Leonel Siqueira <lucasleosiq@gmail.com>
2023-07-25 08:34:18 -0300

    feat: add off button for CRT screen effect on option screen

--- Assets/Code/ToggleScriptOnGameObject.cs
+++ Assets/Code/ToggleScriptOnGameObject.cs
@@ -7,9 +7,17 @@ public class ToggleScriptOnGameObject : MonoBehaviour
 
     private bool scriptEnabled = true;
 
+    private void Start()
+    {
+        ToggleTargetScript();
+    }
+
     [NaughtyAttributes.Button]
     public void ToggleTargetScript()
     {
+        if (MainManager.Instance.CRTEffect == false) MainManager.Instance.CRTEffect = true;
+        else if (MainManager.Instance.CRTEffect == true) MainManager.Instance.CRTEffect = false;
+
         if (targetGameObject != null && scriptToToggle != null)
         {
             scriptEnabled = !scriptEnabled;
